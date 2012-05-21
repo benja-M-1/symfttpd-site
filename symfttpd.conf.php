0e73c7332885dd51434b9a965bc7bd93c300a20f
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2012-05-21 02:05:58 +0200

    Remove inexistant files

--- symfttpd.conf.php
+++ symfttpd.conf.php
@@ -2,8 +2,8 @@
 
 $options['project_type'] = 'php';
 $options['project_readable_dirs'] = array();
-$options['project_readable_files'] = array('humans.txt', 'symfttpd.phar'); // readable files by the server in the web dir (robots.txt).
-$options['project_readable_phpfiles'] = array('index.php', 'index_dev.php'); // executable php files in the web directory
+$options['project_readable_files'] = array('symfttpd.phar'); // readable files by the server in the web dir (robots.txt).
+$options['project_readable_phpfiles'] = array('index.php'); // executable php files in the web directory
 $options['project_readable_restrict'] = true;
 $options['project_web_dir'] = 'web';
 $options['project_log_dir'] = 'log';
